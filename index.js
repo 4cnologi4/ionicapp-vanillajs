ee2f8ccdd609abaed597d01a695f62ad6e3751bc
4cnologi4 <daniell9404@outlook.com>
2020-03-15 21:31:23 -0600

    2 files modified

---
+++ index.js
@@ -0,0 +1,10 @@
+const inputReason = document.querySelector("#input-reason");
+const inputExpense = document.querySelector("#input-expense");
+
+const btnClear = document.querySelector("#btn-clear");
+const btnAdd = document.querySelector("#btn-add");
+
+
+btnAdd.addEventListener('click', () => {
+  alert("Funciona");
+});
